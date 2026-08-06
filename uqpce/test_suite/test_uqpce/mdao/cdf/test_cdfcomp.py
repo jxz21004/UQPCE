f2115bb7d27004d77b082374b2f433329407e275
jnschmid <73538725+jnschmid@users.noreply.github.com>
2026-08-06 12:29:09 -0400

    Updated confidence interval calculation and UQPCEGroup/MultiUQPCEGroup structure (#22)
    * Updated confidence interval calculation to use jax for derivatives. This change leads to less effort on the user-side to determine appropriate tanh parameters, faster code due to the removal of the solver, and higher accuracy due to no user-chosen tanh parameters. The tanh-based confidence interval calculation no longer default, and the jax implementation is recommended.
    
    * Updated documentation to reflect the changes in the differentiable confidence interval calculation.
    
    * UQPCEGroupnow uses promotes_inputs=['responses'] (or promotes_inputs=[('responses', 'your_response_name')]) and only supports a single uncertain response.
    
    * UQPCEGroup no longer uses the option 'uncert_list'.
    
    * MultiUQPCEGroup was added to support multiple responses, and it uses 'uncert_list' as an option.

--- uqpce/test_suite/test_uqpce/mdao/cdf/test_cdfcomp.py
+++ uqpce/test_suite/test_uqpce/mdao/cdf/test_cdfcomp.py
@@ -1,17 +1,14 @@
 import unittest
 
-import matplotlib.pyplot as plt
 import numpy as np
 import openmdao.api as om
-from openmdao.jax_funcs import act_tanh
 from scipy.stats import beta, expon, nbinom
 
-from uqpce.mdao.cdf.cdfgroup import CDFGroup
+from uqpce.mdao.cdf.cdfcomp import CDFComp
 
-tanh_omega = 1e-6
 aleat_cnt = 500_000
 
-class TestCDFGroup(unittest.TestCase):
+class TestCDFComp(unittest.TestCase):
     def setUp(self):
 
         self.sig = 0.05
@@ -34,121 +31,114 @@ class TestCDFGroup(unittest.TestCase):
 
         prob.model.add_subsystem(
             'lower',
-            CDFGroup(
-                alpha=alpha, tail='lower', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='lower', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=self.beta_samples.min(), sample_ref=self.beta_samples.max()
             ), promotes_inputs=['*']
         )
         prob.model.add_subsystem(
             'upper',
-            CDFGroup(
-                alpha=alpha, tail='upper', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='upper', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=self.beta_samples.min(), sample_ref=self.beta_samples.max()
             ), promotes_inputs=['*']
         )
 
         prob.setup()
-        prob.set_val('lower.cdf.samples', self.beta_samples)
-        prob.set_val('upper.cdf.samples', self.beta_samples)
+        prob.set_val('f_sampled', self.beta_samples)
+        prob.set_val('f_sampled', self.beta_samples)
         prob.run_model()
 
         ci_lower = prob.get_val('lower.f_ci')[0]
         ci_upper = prob.get_val('upper.f_ci')[0]
 
         self.assertTrue(
-            np.isclose(ci_lower, np.quantile(self.beta_samples, self.cil), atol=1e-2),
+            np.isclose(ci_lower, np.quantile(self.beta_samples, self.cil), atol=1e-6),
             msg='Beta distribution failed with lower confidence interval.'
         )
         self.assertTrue(
-            np.isclose(ci_upper, np.quantile(self.beta_samples, self.cih), atol=1e-2),
+            np.isclose(ci_upper, np.quantile(self.beta_samples, self.cih), atol=1e-6),
             msg='Beta distribution failed with upper confidence interval.'
         )
 
+
     def test_expon(self):
         alpha = 0.05
-        tanh_omega = 1e-8
 
         prob = om.Problem(reports=False)
 
         prob.model.add_subsystem(
             'lower',
-            CDFGroup(
-                alpha=alpha, tail='lower', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='lower', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=self.expon_samples.min(), sample_ref=self.expon_samples.max()
             ), promotes_inputs=['*']
         )
         prob.model.add_subsystem(
             'upper',
-            CDFGroup(
-                alpha=alpha, tail='upper', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='upper', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=self.expon_samples.min(), sample_ref=self.expon_samples.max()
             ), promotes_inputs=['*']
         )
 
         prob.setup()
-        prob.set_val('lower.cdf.samples', self.expon_samples)
-        prob.set_val('upper.cdf.samples', self.expon_samples)
+        prob.set_val('f_sampled', self.expon_samples)
+        prob.set_val('f_sampled', self.expon_samples)
         prob.run_model()
 
         ci_lower = prob.get_val('lower.f_ci')[0]
         ci_upper = prob.get_val('upper.f_ci')[0]
 
         self.assertTrue(
-            np.isclose(ci_lower, np.quantile(self.expon_samples, self.cil), atol=1e-2),
+            np.isclose(ci_lower, np.quantile(self.expon_samples, self.cil), atol=1e-6),
             msg='Exponential distribution failed with lower confidence interval.'
         )
         self.assertTrue(
-            np.isclose(ci_upper, np.quantile(self.expon_samples, self.cih), atol=1e-2),
+            np.isclose(ci_upper, np.quantile(self.expon_samples, self.cih), atol=1e-6),
             msg='Exponential distribution failed with upper confidence interval.'
         )
 
+
     def test_nbinom(self):
         alpha = 0.05
-        tanh_omega = 1e-10
 
         prob = om.Problem(reports=False)
 
         prob.model.add_subsystem(
             'lower',
-            CDFGroup(
-                alpha=alpha, tail='lower', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='lower', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=float(self.nbinom_samples.min()), sample_ref=float(self.nbinom_samples.max())
             ), promotes_inputs=['*']
         )
         prob.model.add_subsystem(
             'upper',
-            CDFGroup(
-                alpha=alpha, tail='upper', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='upper', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=float(self.nbinom_samples.min()), sample_ref=float(self.nbinom_samples.max())
             ), promotes_inputs=['*']
         )
 
         prob.setup(force_alloc_complex=True)
-        prob.set_val('lower.cdf.samples', self.nbinom_samples)
-        prob.set_val('upper.cdf.samples', self.nbinom_samples)
+        prob.set_val('f_sampled', self.nbinom_samples)
+        prob.set_val('f_sampled', self.nbinom_samples)
         prob.run_model()
 
         ci_lower = prob.get_val('lower.f_ci')[0]
         ci_upper = prob.get_val('upper.f_ci')[0]
 
         self.assertTrue(
-            np.isclose(ci_lower, np.quantile(self.nbinom_samples, self.cil), atol=1e-2),
+            np.isclose(ci_lower, np.quantile(self.nbinom_samples, self.cil), atol=1e-6),
             msg='Negative Binomial distribution failed with lower confidence interval.'
         )
         self.assertTrue(
-            np.isclose(ci_upper, np.quantile(self.nbinom_samples, self.cih), atol=1e-2),
+            np.isclose(ci_upper, np.quantile(self.nbinom_samples, self.cih), atol=1e-6),
             msg='Negative Binomial distribution failed with upper confidence interval.'
         )
 
     def test_combined(self):
         alpha = 0.05
-        tanh_omega = 1e-6
 
         samps = self.beta_samples - self.expon_samples + self.nbinom_samples
 
@@ -156,41 +146,37 @@ class TestCDFGroup(unittest.TestCase):
 
         prob.model.add_subsystem(
             'lower',
-            CDFGroup(
-                alpha=alpha, tail='lower', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='lower', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=samps.min(), sample_ref=samps.max()
             ), promotes_inputs=['*']
         )
         prob.model.add_subsystem(
             'upper',
-            CDFGroup(
-                alpha=alpha, tail='upper', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='upper', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=samps.min(), sample_ref=samps.max()
             ), promotes_inputs=['*']
         )
 
         prob.setup(force_alloc_complex=True)
-        prob.set_val('lower.cdf.samples', samps)
-        prob.set_val('upper.cdf.samples', samps)
+        prob.set_val('f_sampled', samps)
         prob.run_model()
 
         ci_lower = prob.get_val('lower.f_ci')[0]
         ci_upper = prob.get_val('upper.f_ci')[0]
 
         self.assertTrue(
-            np.isclose(ci_lower, np.quantile(samps, self.cil), atol=1e-2),
+            np.isclose(ci_lower, np.quantile(samps, self.cil), atol=1e-6),
             msg='Combined distribution failed with lower confidence interval.'
         )
         self.assertTrue(
-            np.isclose(ci_upper, np.quantile(samps, self.cih), atol=1e-2),
+            np.isclose(ci_upper, np.quantile(samps, self.cih), atol=1e-6),
             msg='Combined distribution failed with upper confidence interval.'
         )
 
     def test_combined_high_order(self):
         alpha = 0.05
-        tanh_omega = 1e-8
 
         samps = self.beta_samples**6 + self.expon_samples**5 - self.nbinom_samples**3
 
@@ -198,35 +184,33 @@ class TestCDFGroup(unittest.TestCase):
 
         prob.model.add_subsystem(
             'lower',
-            CDFGroup(
-                alpha=alpha, tail='lower', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='lower', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=samps.min(), sample_ref=samps.max()
             ), promotes_inputs=['*']
         )
         prob.model.add_subsystem(
             'upper',
-            CDFGroup(
-                alpha=alpha, tail='upper', vec_size=aleat_cnt, tanh_omega=tanh_omega,
+            CDFComp(
+                alpha=alpha, tail='upper', vec_size=aleat_cnt,
                 epistemic_cnt=1, aleatory_cnt=aleat_cnt,
-                sample_ref0=samps.min(), sample_ref=samps.max()
             ), promotes_inputs=['*']
         )
 
         prob.setup(force_alloc_complex=True)
-        prob.set_val('lower.cdf.samples', samps)
-        prob.set_val('upper.cdf.samples', samps)
+        prob.set_val('f_sampled', samps)
+        prob.set_val('f_sampled', samps)
         prob.run_model()
 
         ci_lower = prob.get_val('lower.f_ci')[0]
         ci_upper = prob.get_val('upper.f_ci')[0]
 
         self.assertTrue(
-            np.isclose(ci_lower, np.quantile(samps, self.cil), atol=1e-2),
+            np.isclose(ci_lower, np.quantile(samps, self.cil), atol=1e-6),
             msg='Combined high order failed with lower confidence interval.'
         )
         self.assertTrue(
-            np.isclose(ci_upper, np.quantile(samps, self.cih), atol=1e-2),
+            np.isclose(ci_upper, np.quantile(samps, self.cih), atol=1e-6),
             msg='Combined high order failed with upper confidence interval.'
         )
 
